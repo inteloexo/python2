5ae00f157b28c2aa2c56272a914261003215de82
inteloexo <inteloexo@gmail.com>
2014-03-26 17:09:49 +0200

    New file filesStats.py created

--- filesStats.py
+++ filesStats.py
@@ -1,1 +1,5 @@
-__author__ = 'mantas'
+#Apskaičiuoti nurodytoje direktorijoje esančių failų statistiką ir ją išvesti į failą.
+#Statistika: pirmiausia išvesti bendrai visuose failuose kokie ir kiek kartų
+#pasikartoja a) žodžiai b) simboliai. Antra, į tą patį failą išvesti tą pąčią
+#statistiką apskaičiuotą kiekvienam failui atskirai.
+#Author - Mantas Gedrimas
